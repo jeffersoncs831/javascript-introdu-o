953dde4e09703b9bede22eebdb328e8cf7cc27a0
jeffersoncs831 <izael1910@gmail.com>
2025-07-04 06:14:06 -0300

    execercios sobre drawer

---
+++ exercicio.js
@@ -0,0 +1,30 @@
+// 1 verificacäo de idade
+
+// let idade = 20;
+// if(idade >= 18){
+// console.log("É maior de idade")
+// }else{
+//     console.log("É menor de idade")
+// }
+
+// 2 Numero positivo, Negativo ou zero
+// let numero = -2
+
+// if(numero > 0){
+//     console.log(`${numero} é positivo `);
+// }else if(numero < 0 ){
+//     console.log(`${numero} é negativo`);
+// }else{
+//     console.log(`é zero`);
+// }
+
+// 3 Maior entre dois numeros
+let numero1 = 100
+let numero2 = 150
+if(numero1 > numero2){
+    console.log(numero1)
+}else if( numero1 >= numero2){
+    console.log("Os numeros sao iguais")
+    
+}
+
